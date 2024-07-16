cecd91ea67d7a242a910a8c5acfe9406653df292
rafimahrus <rafi.mahrus@sigma.co.id>
2024-07-16 16:12:28 +0700

    up from office

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,10 +5,28 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { {"nvim-lua/plenary.nvim"},
+   -- {
+--  'nvim-telescope/telescope-fzf-native.nvim',
+--     build = function(plugin)
+--     local obj = vim.system({'cmake', '-S.', '-Bbuild', '-DCMAKE_BUILD_TYPE=Release'}, {cwd = plugin.dir}):wait()
+--     if obj.code ~= 0 then error(obj.stderr) end
+--        obj = vim.system({'cmake', '--build', 'build', '--config', 'Release'}, {cwd = plugin.dir}):wait()
+--      if obj.code ~= 0 then error(obj.stderr) end
+--        obj = vim.system({'cmake', '--install', 'build', '--prefix', 'build'}, {cwd = plugin.dir}):wait()
+--      if obj.code ~= 0 then error(obj.stderr) end
+--        end
+--      },
+    },
     config = function()
       require("telescope").setup({
         extensions = {
+--           ['fzf'] = {
+--            fuzzy = true,                    -- false will only do exact matching
+--            override_generic_sorter = true,  -- override the generic sorter
+--            override_file_sorter = true,     -- override the file sorter
+--            case_mode = 'smart_case',        -- or 'ignore_case' or 'respect_case'
+--          },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
@@ -19,6 +37,7 @@ return {
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
+--      require("telescope").load_extension('fzf')
       require("telescope").load_extension("ui-select")
     end,
   },
