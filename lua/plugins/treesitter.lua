906045c725c65c5c769ae8645bc3e3228e268f56
rafimahrus <rafi.mahrus@sigma.co.id>
2024-07-18 16:00:04 +0700

    hahahha fuck youuuuuuuu

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,13 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter-context",
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
         highlight = { enable = true },
+        context = { enable = true },
         indent = { enable = true },
       })
     end
