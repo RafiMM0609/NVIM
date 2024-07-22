d4a8cd89da7b1b70a5a663bfc98e0ee3230b6577
Rafi Mahrus <75168126+RafiMM0609@users.noreply.github.com>
2024-07-22 21:33:11 +0700

    hahhaa find you

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,7 +14,11 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- Copy code
-vim.keymap.set('v', 'Y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('v', 'H', '"+y', { noremap = true, silent = true })
+if vim.fn.maparg('B', 'n') ~= '' then
+  vim.api.nvim_del_keymap('n', 'B')
+end
+vim.keymap.set('n', 'B', '"*p', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Space>y', 'yyP', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<Space>y', 'yP', { noremap = true, silent = true })
 
@@ -23,10 +27,7 @@ vim.api.nvim_set_keymap('n', '<Space>j', '<cmd>lua vim.lsp.buf.definition()<CR>'
 
 -- Shortcut Command
 vim.api.nvim_set_keymap('n', '<leader>m', ':Mason<CR>', { noremap = true, silent = true })
-if vim.fn.maparg('G', 'n') ~= '' then
-  vim.api.nvim_del_keymap('n', 'G')
-end
-vim.api.nvim_set_keymap('n', 'G', ':LazyGitCurrentFile<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'L', ':LazyGitCurrentFile<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', 'L', ':LazyGitCurrentFile<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '?', ':Gitsigns blame_line<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'p', '"+p', { noremap = true, silent = true })
@@ -48,23 +49,23 @@ vim.api.nvim_set_keymap('n', '<S-Right>', '6l', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<S-Left>', '6h', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Down>', '6j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Up>', '6k', { noremap = true, silent = true })
-if vim.fn.maparg('K', 'n') ~= '' then
-  -- vim.api.nvim_del_keymap('n', 'K')
-  vim.keymap.del('n', 'K')
-end
-vim.api.nvim_set_keymap('n', 'K', '6k', { noremap = true, silent = true })
-if vim.fn.maparg('J', 'n') ~= '' then
-  vim.api.nvim_del_keymap('n', 'J')
-end
-vim.api.nvim_set_keymap('n', 'J', '6j', { noremap = true, silent = true })
-if vim.fn.maparg('L', 'n') ~= '' then
-  vim.api.nvim_del_keymap('n', 'L')
-end
-vim.api.nvim_set_keymap('n', 'L', '6l', { noremap = true, silent = true })
-if vim.fn.maparg('H', 'n') ~= '' then
-  vim.api.nvim_del_keymap('n', 'H')
-end
-vim.api.nvim_set_keymap('n', 'H', '6h', { noremap = true, silent = true })
+-- if vim.fn.maparg('K', 'n') ~= '' then
+--   -- vim.api.nvim_del_keymap('n', 'K')
+--   vim.keymap.del('n', 'K')
+-- end
+-- vim.api.nvim_set_keymap('n', 'K', '6k', { noremap = true, silent = true })
+-- if vim.fn.maparg('J', 'n') ~= '' then
+--   vim.api.nvim_del_keymap('n', 'J')
+-- end
+-- vim.api.nvim_set_keymap('n', 'J', '6j', { noremap = true, silent = true })
+-- if vim.fn.maparg('L', 'n') ~= '' then
+--   vim.api.nvim_del_keymap('n', 'L')
+-- end
+-- vim.api.nvim_set_keymap('n', 'L', '6l', { noremap = true, silent = true })
+-- if vim.fn.maparg('H', 'n') ~= '' then
+--   vim.api.nvim_del_keymap('n', 'H')
+-- end
+-- vim.api.nvim_set_keymap('n', 'H', '6h', { noremap = true, silent = true })
 
 
 -- For visual mode
